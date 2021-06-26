befc73c641030682b3e80f255525ef505572efcd
Eugene <rageofgods@gmail.com>
2021-06-26 12:14:11 +0300

    init commit

---
+++ Jenkinsfile
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Terraform plan') {
+            steps {
+                sh 'terraform init'
+                sh 'terraform plan -out myplan'
+            }
+        }
+
+        stage('Deploy yc infra') {
+            steps {
+                script {
+                    withCredentials([
+                        string(credentialsId: 'yc_token', variable: 'yc_token'),
+                        string(credentialsId: 'cloud_id', variable: 'cloud_id'),
+                        string(credentialsId: 'folder_id', variable: 'folder_id'),
+                        string(credentialsId: 'ssh_key_pub', variable: 'ssh_key_pub')
+                        ])
+                }
+                sh 'terraform apply -input=false myplan -var "yc_token=${yc_token}" -var "yc_cloud_id=${cloud_id}" -var "yc_folder_id=${folder_id}" -var "ssh_key=${ssh_key_pub}"'
+            }
+        }
+    }
+}
---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+variable "yc_token" {}
+variable "yc_cloud_id" {}
+variable "yc_folder_id" {}
+
+provider "yandex" {
+  token     = "${var.yc_token}"
+  cloud_id  = "${var.yc_cloud_id}"
+  folder_id = "${var.yc_folder_id}"
+  zone      = "ru-central1-b"
+}
+
+variable "ssh_key" {}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8g0dj6sus84bcku631"
+      size     = "20"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_key}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "subnet-1" {
+  value = yandex_vpc_subnet.subnet-1.id
+}
