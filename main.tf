6a98684d83cb9792d4dd64cd92d59339d35678db
Eugene <rageofgods@gmail.com>
2021-07-21 11:39:59 +0300

    tf test

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ variable "yc_vm_cores" {default = "2"} # VM cpu cores count
 variable "yc_vm_memory" {default = "4"} # VM memory in GB
 variable "yc_image_id" {default = "fd8g0dj6sus84bcku631"} # yc compute image list --folder-id standard-images
 variable "anbl_plbk_name" {default = "jenkins-provision"} # which playbook we want to run after inf provision
+variable "yc_vm_platform" {default = "standard-v2"} #https://cloud.yandex.ru/docs/compute/concepts/vm-platforms
 
 provider "yandex" {
   token     = "${var.yc_token}"
@@ -30,6 +31,7 @@ provider "yandex" {
 
 resource "yandex_compute_instance" "vm-1" {
   name = "${var.yc_vm_name}"
+  platform_id = "${var.yc_vm_platform}"
   hostname = "${var.yc_vm_name}"
   allow_stopping_for_update = true # Debug only!
 
@@ -47,14 +49,14 @@ resource "yandex_compute_instance" "vm-1" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true # For external communications
+    nat       = true # Auto create external ip (with nat rule pointing to internal VM ip address
   }
 
   metadata = {
     user-data = "#cloud-config\nusers:\n  - name: ${var.ssh_username}\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ${var.ssh_key}"
   }
 
-  provisioner "remote-exec" {
+  /*provisioner "remote-exec" {
     inline = ["uname -a"]
 
     connection {
@@ -68,7 +70,7 @@ resource "yandex_compute_instance" "vm-1" {
   provisioner "local-exec" {
       command = "ansible-playbook -u ${var.ssh_username} -i '${self.network_interface.0.nat_ip_address},' --private-key ${var.ssh_private_key} --extra-vars 'jenkins_hostname=${self.network_interface.0.nat_ip_address}' jenkins-provision.yaml"
   }
-}
+}*/
 
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
